a663b7ec56ae6d3e750f5f6f3e7abcaa76a9b3a5
caeser lettuce <84699501+caeserlettuce@users.noreply.github.com>
2023-10-17 11:12:28 -0700

    new stuff
    added mobile support (when on mobile, things get sized differently so that it actually looks good and stuff), added aurora to the credits, made it so when you manually slide the image gallery itll wait 5 seconds at least before autosliding it. I also fixed all the stuff anne asked me to fix

--- main.js
+++ main.js
@@ -3,22 +3,25 @@
 var slideimgs = [
   {
     "image": "/assets/siteimg/fbrawl_art1.jpg",
-    "description": "Fruit Brawl concept art"
+    "description": "FruitBrawl concept art"
   },
   {
     "image": "/assets/siteimg/fbrawl-3.jpg",
-    "description": "Fruit Brawl play testing"
+    "description": "FruitBrawl playtesting"
   },
   {
     "image": "/assets/siteimg/fruitbrawl-coin.png",
-    "description": "Fruit Brawl logo on a wooden coin!"
+    "description": "FruitBrawl logo on a wooden coin"
   },
   {
     "image": "/assets/siteimg/fbrawl-2.jpg",
-    "description": "Fruit Brawl Kiwi build"
+    "description": "FruitBrawl Kiwi build"
   }
 ]
 
+
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 
 var slideimgs_length = slideimgs.length;
@@ -26,29 +29,63 @@ var slideimgs_slide = document.getElementById("slideimgs-slide");
 var slideimgs_index = 0;
 var slide_transition = 2000; // anne reference ?????
 var slsf = 100; // size factor (doensnt apply to arrows and will screw things up now)
+var desktop = true;
+var prev_desktop = true;
+var clientmode_interval;
+var dont_autoslide = false;
+var dont_autoslide_timeout;
+
+if (windowWidth < windowHeight) {
+  desktop = false;
+}
 
 function init_slideimgs() {
   var add_html = "";
   var slideimgs_pushed = [...slideimgs];
   slideimgs_pushed.push(slideimgs[0]);
-  document.getElementById("slideimgs").setAttribute("viewBox", `0 0 ${2 * slsf} ${1 * slsf}`)
+  if (desktop == false) { // mobile stuffs
+    document.getElementById("slideimgs").setAttribute("viewBox", `0 0 ${1 * slsf} ${2 * slsf}`)
+  } else {
+    document.getElementById("slideimgs").setAttribute("viewBox", `0 0 ${2 * slsf} ${1 * slsf}`)
+  }
+  
   for (i in slideimgs_pushed) {
-    add_html += `
+    if (desktop == false) { // mobile
+      add_html += `
+<image href="${slideimgs_pushed[i]["image"]}" x="${i * (1 * slsf)}" width="${1 * slsf}" height="${2 * slsf}" />
+<foreignObject x="${i * (2 * slsf)}" y="0" width="${1 * slsf}" height="${2 * slsf}">
+  <xhtml:div class="slide-description ctr1"><xhtml:span class="sldesc ctr2">${slideimgs_pushed[i]["description"].replaceAll("\n", "<br>")}</xhtml:span></xhtml:div>
+</foreignObject>
+`; 
+    } else {
+      add_html += `
 <image href="${slideimgs_pushed[i]["image"]}" x="${i * (2 * slsf)}" width="${2 * slsf}" height="${1 * slsf}" />
 <foreignObject x="${i * (2 * slsf)}" y="0" width="${2 * slsf}" height="${1 * slsf}">
   <xhtml:div class="slide-description ctr1"><xhtml:span class="sldesc ctr2">${slideimgs_pushed[i]["description"].replaceAll("\n", "<br>")}</xhtml:span></xhtml:div>
 </foreignObject>
 `; 
+    }
+    
   }
   //add_html += `<image href="${slideimgs[0]}" x="${slideimgs_length * 20}" width="20" height="10" />\n`;
+
   slideimgs_slide.innerHTML = add_html;
   slideimgs_slide.style.transitionDuration = `${slide_transition}ms`;
 }
 
 init_slideimgs();
 
-function slide_images(direction) {
+function slide_images(direction, no_timeout) {
   console.log(slideimgs_index);
+  if (no_timeout != true) {
+    dont_autoslide = true;
+    clearTimeout(dont_autoslide_timeout);
+    dont_autoslide_timeout = setTimeout(() => {
+      dont_autoslide = false;
+      clearTimeout(dont_autoslide_timeout);
+    }, 5000);  
+  }
+  
   if (slideimgs_index + direction > slideimgs_length) {  // if its going past the end (at the end, going farther)
     // remove transition, skip to beginning, add transition, then slide forward (seamless)
     slideimgs_slide.style.transitionDuration = `0ms`;
@@ -57,30 +94,49 @@ function slide_images(direction) {
       slideimgs_slide.style.transitionDuration = `${slide_transition}ms`;
       slideimgs_index = 0;
       slideimgs_index += direction;
-      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+      if (desktop == false) { // mobile
+        slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(1 * slsf)}px)`;
+      } else {
+        slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+      }
+      
     }, 10);
   } else if (slideimgs_index + direction < 0) {           // if its going past the beginning (at the beginning, going earlier)
     // slide backward, if its going to go behind 0, remove transition, skip to end, add transition, and slide backward (also seamless)
     slideimgs_slide.style.transitionDuration = `0ms`;
-    slideimgs_slide.style.transform = `translateX(${slideimgs_length * -(2 * slsf)}px)`;
+    if (desktop == false) { // mobile
+      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(1 * slsf)}px)`;
+    } else {
+      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+    }
     setTimeout( () => {
       slideimgs_slide.style.transitionDuration = `${slide_transition}ms`;
       slideimgs_index = slideimgs_length;
       slideimgs_index += direction;
-      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+      if (desktop == false) { // mobile
+        slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(1 * slsf)}px)`;
+      } else {
+        slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+      }
     }, 10);
 
   } else {
     slideimgs_index += direction;
-    slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+    if (desktop == false) { // mobile
+      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(1 * slsf)}px)`;
+    } else {
+      slideimgs_slide.style.transform = `translateX(${slideimgs_index * -(2 * slsf)}px)`;
+    }
   }
   console.log(slideimgs_index);
 }
 
 
 var slider_interval = setInterval( () => {
-  slide_images(1);
-}, 10000)
+  if (dont_autoslide == false) {
+    slide_images(1, true);
+  }
+}, 7000)
 
 
 
@@ -97,3 +153,35 @@ for (i in fruitbrawl_team) {
   document.getElementById("team-div").innerHTML += add_html;
 }
 
+
+
+function check_mobile() {
+  windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  if (windowWidth < windowHeight) {
+    desktop = false;
+  } else {
+    desktop = true;
+  }
+
+  if (prev_desktop != desktop) {
+    // run the new stuff
+    console.log("changed");
+    if (desktop == false) {
+      document.getElementById("mobile").setAttribute("href", "../mobile.css");
+    } else {
+      document.getElementById("mobile").setAttribute("href", "");
+    }
+    slideimgs_slide.innerHTML = "";
+    slideimgs_slide.style.transform = `translateX(0px)`;
+    init_slideimgs();
+  }
+
+
+  prev_desktop = desktop;
+}
+
+check_mobile();
+
+
+clientmode_interval = setInterval(check_mobile, 500);
